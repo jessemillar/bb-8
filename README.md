875c2a52c53e919a9085bb38ccb5e4310210953e
Jesse Millar <jesse.millar@microsoft.com>
2020-02-05 13:29:50 -0600

    Adding a Go version of the sentient script
    Signed-off-by: Jesse Millar <jesse.millar@microsoft.com>

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+## Go
+```
+go build main.go && sudo ./main BB-9B05
+```
+
+## Node
 Run `npm install` and then `node [desired_script]`.
 
 ![sentient.js](/gifs/sentient.gif)
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"gobot.io/x/gobot"
+	"gobot.io/x/gobot/platforms/ble"
+	"gobot.io/x/gobot/platforms/sphero/bb8"
+	"gobot.io/x/gobot/platforms/sphero/ollie"
+)
+
+var batteryStates = []string{"charging", "battery ok", "battery low", "battery critical"}
+
+func main() {
+	bleAdaptor := ble.NewClientAdaptor(os.Args[1])
+	bb8 := bb8.NewDriver(bleAdaptor)
+	bb8.SetRGB(0, 0, 0)
+
+	work := func() {
+		gobot.Every(10*time.Second, func() {
+			bb8.Roll(2, uint16(gobot.Rand(200)))
+			// r := uint8(gobot.Rand(255))
+			// g := uint8(gobot.Rand(255))
+			// b := uint8(gobot.Rand(255))
+			// bb8.SetRGB(r, g, b)
+
+			bb8.GetPowerState(func(powerState ollie.PowerStatePacket) {
+				fmt.Println("Battery: " + batteryStates[powerState.PowerState-1])
+			})
+		})
+	}
+
+	robot := gobot.NewRobot("bb",
+		[]gobot.Connection{bleAdaptor},
+		[]gobot.Device{bb8},
+		work,
+	)
+
+	robot.Start()
+}
